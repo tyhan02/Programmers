6b7e1286003b000ed8be4200738fcccae0cd4fb1
Taeyoung Han <tyhan02@naver.com>
2023-05-30 20:00:27 +0900

    [unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int start, int end) {
+
+        int[] answer = new int[end - start + 1];
+        
+        int a = 0; 
+ 
+        for(int i = start; i <= end; i++){
+            answer[a++] = i;
+        }
+        
+        return answer;
+    }
+}
