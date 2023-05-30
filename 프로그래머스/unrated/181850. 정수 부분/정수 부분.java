3dd4f4ebfe968d942be7e8b68522b8f9c3a853ad
Taeyoung Han <tyhan02@naver.com>
2023-05-30 16:41:24 +0900

    [unrated] Title: 정수 부분, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181850. 정수 부분/정수 부분.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(double flo) {
+        int answer = 0;
+        
+        if (flo > 0 ){
+            answer = (int)flo;
+        }
+        
+        
+        return answer;
+    }
+}
