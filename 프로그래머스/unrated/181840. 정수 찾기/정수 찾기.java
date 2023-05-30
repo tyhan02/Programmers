3459233f3694aef5789c9fb6226590979faa6e6a
Taeyoung Han <tyhan02@naver.com>
2023-05-30 16:41:14 +0900

    [unrated] Title: 정수 찾기, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] num_list, int n) {
+       
+        int answer = n;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            
+            if (num_list[i] == n) {
+                answer = 1;
+                break;
+            } else{
+                answer = 0;
+            }
+         }
+         return answer;
+    }
+}
