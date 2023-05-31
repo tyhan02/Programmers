f8a58bf704444b85560c4d1692ae8b9de7ac59de
Taeyoung Han <tyhan02@naver.com>
2023-05-31 15:52:09 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        
+        int[] solution = new int[n];
+            
+            for(int i=0; i<n; i++){
+                solution[i] = num_list[i];
+            }
+        return solution;
+    }
+}
