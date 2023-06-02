f9f29cd6bcc203c29b298892696647cf991f4cf4
Taeyoung Han <tyhan02@naver.com>
2023-06-02 20:47:47 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.57 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,21 +1,16 @@
 class Solution {
     public int solution(int n, String control) {
-        
-        for(int i = 0; i < control.length(); i++)
-            switch(control.charAt(i)){
-            case 'w':
+        for (int i = 0; i < control.length(); i++) {
+            char ch = control.charAt(i);
+            if (ch == 'w') {
                 n += 1;
-                break;
-            case 's':
+            } else if (ch == 's') {
                 n -= 1;
-                break;
-            case 'd':
+            } else if (ch == 'd') {
                 n += 10;
-                break;
-            case 'a':
+            } else if (ch == 'a') {
                 n -= 10;
-                break;
-    
+            }
         }
         return n;
     }
