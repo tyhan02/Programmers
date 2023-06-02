4ce854e0448fcfd447de7f9277b19f1e94deec5c
Taeyoung Han <tyhan02@naver.com>
2023-06-02 17:02:57 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.04 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,10 @@
+class Solution {
+    public float solution(float num1, float num2) {
+        
+        float answer = 0;
+        
+        answer = (int)((num1/num2) * 1000);
+        
+        return answer;
+    }
+}
