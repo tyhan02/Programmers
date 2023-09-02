ff2a58ce75d7ac171345b97ac940f342c9057278
Taeyoung Han <tyhan02@naver.com>
2023-09-02 23:53:10 +0900

    [level 0] Title: A로 B 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.py
@@ -0,0 +1,12 @@
+def solution(before, after):
+    answer = 0
+    cnt = 0
+    for i in before:
+        if before.count(str(i)) == after.count(str(i)):
+            cnt +=1
+    if cnt == len(before):
+        answer =1
+    answser = 0
+
+
+    return answer
